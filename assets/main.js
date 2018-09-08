4f5ad2b10a1e50ca7ed1f3e0aea16e9107bb0c1a
Isaac Chansky <ichansky@gmail.com>
2018-09-08 00:05:08 -0400

    new site who dis

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,23 @@
 (function() {
 
+
+    var throttle = function(func, limit) {
+        var inThrottle
+        return function () {
+            var args = arguments
+            var context = this
+            if (!inThrottle) {
+                func.apply(context, args)
+                inThrottle = true
+                setTimeout(function() {
+                    inThrottle = false;
+                }, limit);
+            }
+        }
+    }
+
+
+
     // smooth scrolling
     var anchorLinks = [].slice.call(document.querySelectorAll('a[href^="#"]'));
 
@@ -14,5 +32,45 @@
         })
     });
 
+
+    // var workheadlines = [].slice.call(document.querySelectorAll('.work-headlines'));
+
+    // workheadlines.forEach(function (el) {
+
+    // });
+    var bodyEl = document.querySelector("body");
+    window.addEventListener('scroll', function() {
+        if (document.documentElement.scrollTop > 50) {
+            bodyEl.classList.add("is-scrolled");
+        } else {
+            bodyEl.classList.remove('is-scrolled');
+        }
+    });
+
+
+
+
+
+
+
+    var workItems = [].slice.call(document.querySelectorAll('.work-headline'));
+    workItems.forEach(function(el) {
+        el.addEventListener('mouseover', function() {
+            document.querySelector('body').classList.add('has-work-active');
+        });
+        el.addEventListener('mouseout', function () {
+            document.querySelector('body').classList.remove('has-work-active');
+        });
+    });
+
+
+    function onScroll() {
+        $('body').toggleClass('is-inverted', document.querySelector('#working').getBoundingClientRect().top < ($(window).height() - 200));
+
+        $('body').toggleClass('is-at-bottom', $(window).scrollTop() + $(window).height() == $(document).height());
+    }
+
+    $(window).scroll(throttle(onScroll, 100));
+    onScroll();
 
 })();
